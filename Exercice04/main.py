c9f8186b2842579ed0ef568ee1778fd82d99df93
ROM-XIX <romain.rozalen@gmail.com>
2025-10-09 15:34:17 +0200

    maj correction exo 4

--- Exercice04/main.py
+++ Exercice04/main.py
@@ -1,4 +1,4 @@
-class myClass:
+class MyClass:
     def __init__(self, full_name):
         self.full_name = full_name
 
@@ -6,7 +6,7 @@ class myClass:
         print("Le nom complet est :", self.full_name)
 
 
-class other_class:
+class Other_Class:
     def __init__(self, first_name, name):
         self.first_name = first_name
         self.name = name
